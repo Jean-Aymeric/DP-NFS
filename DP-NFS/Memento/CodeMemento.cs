ef8f23d2a8d99a2588947039d4029bcef5225c75
Jean-Aymeric Diet <jeanaymeric@gmail.com>
2023-03-30 12:32:16 +0200

    use a record on CodeMemento

--- DP-NFS/Memento/CodeMemento.cs
+++ DP-NFS/Memento/CodeMemento.cs
@@ -1,11 +1,7 @@
 using System;
 
 namespace Memento {
-	public class CodeMemento {
-        public String Text { get; }
-
-        public CodeMemento(String text) {
-			this.Text = text;
-		}
-	}
+	public record CodeMemento (
+        String Text
+	);
 }
